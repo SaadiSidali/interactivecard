6d7d54a33c1c17832461a0d9eba647483489dda8
Sidali <saadi.sidali@outlook.fr>
2022-08-22 22:55:40 +0100

    first commit

---
+++ src/components/CardBack.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="img"></div>
+</template>
+
+<style scoped>
+.img {
+  box-shadow: 1px 2px 14px 4px #00000061;
+  border-radius: 6px;
+  color: var(--white);
+  position: absolute;
+  z-index: 1;
+  left: 250px;
+  top: 340px;
+  height: 150px;
+  width: 273.672px;
+  background-image: url("/images/bg-card-back.png");
+  background-size: cover;
+  background-repeat: no-repeat;
+}
+</style>
